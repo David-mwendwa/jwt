acdd0cd27120aad32f2333754322e1f760e2b6ef
David-mwendwa <klatzdavie19@gmail.com>
2022-02-10 17:58:34 +0300

    sign json web token

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,17 @@
-const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken');
 
+const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
-  const {username, password} = req.body
-  if(!username || !password) {
-    throw new CustomAPIError('Please provide email and password', 400)
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
   }
-  console.log(username, password)
-  res.send('Fake login/register');
+
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn: '30d'});
+  console.log(id, token)
+  res.status(200).json({msg: 'user created', token})
 };
 
 const dashboard = async (req, res) => {
